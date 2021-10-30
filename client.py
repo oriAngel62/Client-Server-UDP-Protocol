244d33600922abf2583e744006a61e83caf5c857
ori <oriangel62@gmail.com>
2021-10-30 13:19:09 +0300

    1

---
+++ client.py
@@ -0,0 +1,6 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.sendto(b'Ori Angel 314617739', ('127.0.0.1', 12345))
+data, addr = s.recvfrom(1024)
+print(str(data), addr)
+s.close()
