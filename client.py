3c540cf75a1551d1046e2f559e565400a9ea86e1
Ori <oriangel62@gmail.com>
2021-11-02 22:07:53 +0200

    one side works

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+from datetime import date, time
 import socket
 import sys
 from typing import BinaryIO
@@ -10,23 +11,34 @@ f = open(textinfo, "rb")
 # print(f.read())
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-chunckSize = 100
+chunckSize = 6
 
 stringFile = f.read()
 #file is to big
 if len(stringFile) > 50000:
     print("file is to big")
 else:
-    # print(len(stringFile))
-    # listChuncks = []
-    # for i in range(0, len(stringFile), chunckSize):
-    #     listChuncks.append(stringFile[i:i + chunckSize])
-    # #send the chuncks of data to foo
-    # for chuncks in listChuncks:
-    #     s.sendto(chuncks, (FOO_IP, FOO_PORT))
-    # print(len(listChuncks[1]))
-    # s.settimeout(8.0)
-    s.sendto(stringFile, (FOO_IP, FOO_PORT))
-    data, addr = s.recvfrom(1024)
+    print(len(stringFile))
+    listChuncks = []
+    for i in range(0, len(stringFile) - 1, chunckSize):
+        listChuncks.append(stringFile[i:i + chunckSize])
+    #send the chuncks of data to foo
+    packageNum = 0
+    s.settimeout(0.1)
+    for chuncks in listChuncks:
+        while True:
+            try:
+                s.sendto(
+                    str(packageNum).zfill(3).encode() + chuncks,
+                    (FOO_IP, FOO_PORT))
+                data, addr = s.recvfrom(1024)
+                print("yes")
+                break
+            except socket.timeout:
+                print("no")
+
+        print(data)
+        packageNum = packageNum + 1
+
 f.close()
 s.close()
--- server.py
+++ server.py
@@ -1,13 +1,25 @@
+from datetime import date
 import socket
 import sys
 
 SERVER_PORT = int(sys.argv[1])
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', SERVER_PORT))
+# s.settimeout(0.5)
+# while True:
+#     try:
+#         data, addr = s.recvfrom(1024)
+#         print(data[:3].decode())
+#         print(str(data), addr)
+#         # s.settimeout(8.0)
+#         while True:
+#             s.sendto(b"y", addr)
+#             break
+#     except socket.timeout:
+#         continue
+
 while True:
-    # s.settimeout(8.0)
     data, addr = s.recvfrom(1024)
-    # sock.settimeout(None)
-    # print(len(data))
+    print(data[:3].decode())
     print(str(data), addr)
     s.sendto(data, addr)
